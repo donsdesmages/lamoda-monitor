427d54228d8904a8ef3807a9a6edeebe5f003c52
donsdesmages <magesbeatss@gmail.com>
2024-05-13 21:56:02 +0300

    Empty project (first commit)

---
+++ src/main/java/com/example/lamodamonitor/LamodaMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.lamodamonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LamodaMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LamodaMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lamodamonitor/LamodaMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lamodamonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LamodaMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
