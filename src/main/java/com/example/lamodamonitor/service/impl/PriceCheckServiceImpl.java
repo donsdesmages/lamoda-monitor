2a63909a4f7bb011f1e35afa9077e8b1e94b202e
donsdesmages <magesbeatss@gmail.com>
2024-05-16 17:06:58 +0300

    Added Thread.start (Working version for lamoda parsing)

--- src/main/java/com/example/lamodamonitor/service/impl/PriceCheckServiceImpl.java
+++ src/main/java/com/example/lamodamonitor/service/impl/PriceCheckServiceImpl.java
@@ -2,7 +2,6 @@ package com.example.lamodamonitor.service.impl;
 
 import com.example.lamodamonitor.model.MonitorResponseDto;
 import com.example.lamodamonitor.repository.ProductRepository;
-import com.example.lamodamonitor.service.MonitorService;
 import com.example.lamodamonitor.service.PriceCheckService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
